cdccc7425e9255e2d9c47961187c530518847ed6
Offset <offsetpaladin89ttv@gmail.com>
2024-03-24 10:37:13 -0700

    Hologram Changes
    Made hologram 1 entity and modified the range of Y and h motion

--- src/main/java/me/offsetpaladin89/MoreArmors/handlers/HologramHandler.java
+++ src/main/java/me/offsetpaladin89/MoreArmors/handlers/HologramHandler.java
@@ -26,63 +26,33 @@ public class HologramHandler {
         HologramHandler.plugin = plugin;
     }
 
-    public void createHologram(Location playerLoc, Location loc, String s, Long delay) {
+    public void createDamageHologram(Location playerLoc, Location loc, String s, Long delay) {
 
-        Vector direction = playerLoc.getDirection().normalize();
-        Location spawnLoc = playerLoc.add(loc.toVector().subtract(playerLoc.toVector()).multiply(0.65)).subtract(0, 0.5, 0);
+        Vector dir = playerLoc.getDirection().normalize();
+        Location spawnLoc = playerLoc.add(loc.toVector().subtract(playerLoc.toVector()).multiply(0.65));
 
         Random random = new Random();
-        double offsetX = (random.nextDouble() * 1.5 - 0.75);
-        double offsetY = (random.nextDouble() - 0.5);
+        Vector hDir = new Vector(dir.getZ(), 0, -dir.getX()).normalize();
+        spawnLoc.add(hDir.multiply(random.nextDouble() * 1.5 - 0.75)).add(0, random.nextDouble() * 1.5 - 0.5, 0);
 
-        Vector horizontalDirection = new Vector(direction.getZ(), 0, -direction.getX()).normalize();
-        spawnLoc.add(horizontalDirection.multiply(offsetX));
-        spawnLoc.add(new Vector(0, offsetY, 0));  // Vertical movement
-
-        Entity main = loc.getWorld().spawnEntity(spawnLoc, EntityType.BAT);
-        Entity rider = loc.getWorld().spawnEntity(spawnLoc, EntityType.ARMOR_STAND);
-
-        LivingEntity bat = (LivingEntity) main;
-        ArmorStand armorStand = (ArmorStand) rider;
-
-        setPersistentData(armorStand, "HologramEntity", true);
-        setPersistentData(bat, "HologramEntity", true);
-
-        configureBat(bat, armorStand);
-        configureArmorStand(armorStand, s);
+        Entity init = loc.getWorld().spawnEntity(spawnLoc, EntityType.ARMOR_STAND);
+        ArmorStand armorStand = (ArmorStand) init;
+        PersistentDataContainer pdc = armorStand.getPersistentDataContainer();
+        pdc.set(new NamespacedKey(plugin, "HologramEntity"), PersistentDataType.BOOLEAN, true);
+        armorStand.setSmall(true);
+        armorStand.setInvisible(true);
+        armorStand.setMarker(true);
+        armorStand.setGravity(false);
+        armorStand.setCustomNameVisible(true);
+        armorStand.setCustomName(plugin.convertColoredString(s));
 
         new BukkitRunnable() {
-            public void run() {
-                bat.remove();
-                armorStand.remove();
-            }
+            public void run() { armorStand.remove(); }
         }.runTaskLater(plugin, delay);
     }
 
-    private void setPersistentData(Entity entity, String keyName, boolean value) {
-        PersistentDataContainer pdc = entity.getPersistentDataContainer();
-        NamespacedKey key = new NamespacedKey(plugin, keyName);
-        pdc.set(key, PersistentDataType.BOOLEAN, value);
-    }
-
-    private void configureArmorStand(ArmorStand armorStand, String s) {
-        armorStand.setSmall(true);
-        armorStand.setInvisible(true);
-        armorStand.setInvulnerable(true);
-        armorStand.setCustomNameVisible(true);
-        armorStand.setCustomName(plugin.convertColoredString(s));
-    }
-
-    private void configureBat(LivingEntity bat, ArmorStand armorStand) {
-        bat.setInvisible(true);
-        bat.setInvulnerable(true);
-        bat.setSilent(true);
-        bat.setAI(false);
-        bat.addPassenger(armorStand);
-    }
-
     public void createDamageHologram(Location playerLoc, Location loc, Long duration, Double damage) {
-        createHologram(playerLoc, loc, "&7" + new DecimalFormat("#.#").format(damage), duration);
+        createDamageHologram(playerLoc, loc, "&7" + new DecimalFormat("#.#").format(damage), duration);
     }
 
 }
